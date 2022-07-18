f20fd87f2736a3c98d10ae1cc92e992a73ca15da
sammigd <sdean39@gmail.com>
2022-07-18 10:57:53 -0400

    push some fixes

--- R/functions.R
+++ R/functions.R
@@ -1,16 +1,17 @@
 run_model <- function(df,list_cov,list_out)
   {## Convert columns of list_cov to factors
-  col_names <- list_cov
+  col_names <- list_cov[2:length(list_cov)]
   df[col_names] <- lapply(df[col_names], factor)
   df$race_code <- relevel(df$race_code,ref="W")
   df$PCV_combined <- relevel(df$PCV_combined,ref="U")
   ## Create df of response and non-responses to be fed to the logistic regression
-  df <- df[df$COVID_severity %in% list_out,]
-  data1 <- df[df$COVID_severity == list_out[1],]
-  data2 <- df[df$COVID_severity ==list_out[2],]
-  data_comb<-merge(data1,data2,by=c("year_month",list_cov))
+  df1 <- df[df$COVID_severity %in% list_out,]
+  data1 <- df1[df1$COVID_severity == list_out[1],]
+  data2 <- df1[df1$COVID_severity ==list_out[2],]
+  #list_cov_tot <- paste(c("year_month",list_cov))
+  data_comb<-merge(data1,data2,all.x=TRUE,all.y=TRUE)
   ## 
-  if(sum(df$count)==sum(data_comb$count.x)+sum(data_comb$count.y)){print("OK")}
+  if(sum(df1$count)==sum(data_comb$count.x)+sum(data_comb$count.y)){print("OK")}
   else print("ERROR")
   ## Run logistic regression model
   data_comb$COVID_severity.x<-NULL
